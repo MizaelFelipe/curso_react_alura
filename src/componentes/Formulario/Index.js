70025dfed55fc9c1360954cfbefebaa8ab7dd799
Mizael Felipe <mizaelfelipe@outlook.com>
2022-09-20 21:31:31 -0300

    Personalisando e adicionando ligas

--- src/componentes/Formulario/Index.js
+++ src/componentes/Formulario/Index.js
@@ -1,15 +1,27 @@
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 
+
 const Formulario = () => {
+
+    const times = [
+        'Desafiante' , 
+        'Grao mestre' , 
+        'Mestre',
+        'Diamante',
+        'Platina' 
+    ]
+
     return (
         <section className='formulario'>
             <form>
-                <h2>Preencha os dados para criar o cardo do colaborador</h2>
-                <CampoTexto label='Nome' placeholder='Digite o seu nome' />
-                <CampoTexto label='Nome' placeholder='Digite o seu cargo' />
-                <CampoTexto label='Nome' placeholder='Digite o endereco da imagem' />
+                <h2>Lista de Mono Jhin no Brasil</h2>
+                <CampoTexto label='Nome de invocador' placeholder='Digite seu nick de mono Jhin' />
+                <CampoTexto label='posição' placeholder='Qual sua posição jogando de Jhin' />
+                <CampoTexto label='Elo' placeholder='Qual o seu elo maximo sendo mono Jhin' />
+                <ListaSuspensa label = 'Ligas' itens = {times}/>
             </form>
         </section>
     )
