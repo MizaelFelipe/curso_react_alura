d5965087e42929aebd2a6169a716bb47b3fe3e5c
Mizael Felipe <mizaelfelipe@outlook.com>
2022-09-21 20:34:11 -0300

    adicionando funcao ao botao

--- src/componentes/Formulario/Index.js
+++ src/componentes/Formulario/Index.js
@@ -15,14 +15,19 @@ const Formulario = () => {
         'Platina' 
     ]
 
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('form foi submetido');
+    }
+
     return (
         <section className='formulario'>
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Lista de Mono Jhin no Brasil</h2>
-                <CampoTexto label='Nome de invocador' placeholder='Digite seu nick de mono Jhin' />
-                <CampoTexto label='posição' placeholder='Qual sua posição jogando de Jhin' />
+                <CampoTexto obrigatorio = {true} label='Nome de invocador' placeholder='Digite seu nick de mono Jhin' />
+                <CampoTexto obrigatorio = {true} label='posição' placeholder='Qual sua posição jogando de Jhin' />
                 <CampoTexto label='Elo' placeholder='Qual o seu elo maximo sendo mono Jhin' />
-                <ListaSuspensa label = 'Ligas' itens = {times}/>
+                <ListaSuspensa obrigatorio = {true} label = 'Ligas' itens = {times}/>
                 <Botao>
                     Criar card
                 </Botao>
