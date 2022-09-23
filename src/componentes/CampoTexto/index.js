04c1493518106e53eaf66e0b1cdb9797220abe12
Mizael Felipe <mizaelfelipe@outlook.com>
2022-09-22 23:15:39 -0300

    usando useState para armazenas o que foi digitado

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,14 +1,23 @@
+import { useState } from 'react'
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
-    
+
+
+
+    const aoDigitado = (evento) => {
+
+        props.aoAlterado(evento.target.value)
+
+    }
+
     return (
         <div className="campo-texto">
             <label>
                 {props.label}
-                </label>
-            <input required = {props.obrigatorio} placeholder={props.placeholder}/>
-             
+            </label>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder} />
+
         </div>
     )
 }
--- src/componentes/Formulario/Index.js
+++ src/componentes/Formulario/Index.js
@@ -2,32 +2,60 @@ import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
+import { useState } from 'react'
 
 
 
 const Formulario = () => {
 
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [time, setTime] = useState('')
+
+
     const times = [
-        'Desafiante' , 
-        'Grao mestre' , 
+        'Desafiante',
+        'Grao mestre',
         'Mestre',
         'Diamante',
-        'Platina' 
+        'Platina'
     ]
 
     const aoSalvar = (evento) => {
         evento.preventDefault()
-        console.log('form foi submetido');
+        console.log('form foi submetido', nome, cargo, imagem,time);
     }
 
     return (
         <section className='formulario'>
             <form onSubmit={aoSalvar}>
                 <h2>Lista de Mono Jhin no Brasil</h2>
-                <CampoTexto obrigatorio = {true} label='Nome de invocador' placeholder='Digite seu nick de mono Jhin' />
-                <CampoTexto obrigatorio = {true} label='posição' placeholder='Qual sua posição jogando de Jhin' />
-                <CampoTexto label='Elo' placeholder='Qual o seu elo maximo sendo mono Jhin' />
-                <ListaSuspensa obrigatorio = {true} label = 'Ligas' itens = {times}/>
+                <CampoTexto obrigatorio={true}
+                    label='Nome de invocador'
+                    placeholder='Digite seu nick de mono Jhin'
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)}
+                />
+                <CampoTexto obrigatorio={true}
+                    label='posição'
+                    placeholder='Qual sua posição jogando de Jhin'
+                    valor={cargo}
+                    aoAlterado={valor => setCargo(valor)}
+                />
+                <CampoTexto
+                    label='Nome do seu duo'
+                    placeholder='Qual o Nome de invocador do seu duo'
+                    valor={imagem}
+                    aoAlterado={valor => setImagem(valor)} />
+                <ListaSuspensa
+                    obrigatorio={true}
+                    label='Ligas'
+                    itens={times}
+                    valor={time}
+                    aoAlterado={valor => setTime(valor)} />
+
+                
                 <Botao>
                     Criar card
                 </Botao>
