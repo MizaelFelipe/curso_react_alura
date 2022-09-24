868bb71967950132f5fb5a2474ee4258e3e177e3
Mizael Felipe <mizaelfelipe@outlook.com>
2022-09-23 21:18:38 -0300

    stylisando os tiers

--- src/App.js
+++ src/App.js
@@ -6,6 +6,34 @@ import Time from './componentes/Times';
 
 function App() {
 
+  const times = [
+    {
+      nome: 'Desafiante',
+      corPrimaria: '#F3F51D',
+      corSecundaria: '#F5F0A7'
+    },
+    {
+      nome: 'Grao mestre',
+      corPrimaria: '#F50D53',
+      corSecundaria: '#F55B65'
+    },
+    {
+      nome: 'Mestre',
+      corPrimaria: '#CA1FF5',
+      corSecundaria: '#DFA2F5'
+    },
+    {
+      nome: 'Diamante',
+      corPrimaria: '#1FC7FF',
+      corSecundaria: '#ABE9FF'
+    },
+    {
+      nome: 'Platina',
+      corPrimaria: '#14E629',
+      corSecundaria: '#A8E9B8'
+    }
+  ]
+
   const [invocadores,setInvocadores] = useState ([])
   const aoNovoInvocadorAdicionado = (invocador) => {
     console.log(invocador);
@@ -16,9 +44,7 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario aoInvocadorCdastrado = {invocador => aoNovoInvocadorAdicionado(invocador )}/>
-      <Time nome = 'Desafiante' />
-      <Time nome = 'Grao Mestre' />
-      <Time nome = 'Mestre' />
+      {times.map (time => <Time key={time.nome} nome ={time.nome} corPrimaria={time.corPrimaria} corSecundaria= {time.corSecundaria} />)}
       
     </div>
 
