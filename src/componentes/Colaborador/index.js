feb704c4cb77b95933d976ee7cb260e283a9f040
Mizael Felipe <mizaelfelipe@outlook.com>
2022-09-26 20:47:52 -0300

    adicionando colaboradores

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,19 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (<div className='colaborador'>
+        <div className='cabecalho'>
+            <img className='img' src='https://github.com/MizaelFelipe.png' alt='Mizael Felipe'/>
+        </div>
+
+        <div className='rodape'>
+            <h4>Mizael Felipe</h4>
+
+            <h5> Platina </h5>
+        </div>
+    </div>
+
+    )
+}
+
+export default Colaborador
